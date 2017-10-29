c0cf251d0e1829ecf2cd506dd16c9483b60ee27b
brijesh2020 <brijeshtiwari2020@gmail.com>
2017-10-29 23:04:03 +0530

    First commit

---
+++ src/main/java/com/ed/SpringSecuritySpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class SpringSecuritySpringBootApplication extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySpringBootApplication.class, args);
+	}
+}
